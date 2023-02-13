45f8ab163179b42ec5b853c155508adb4f979a56
Sam Robinson <110217039+SamRobinson-2511@users.noreply.github.com>
2023-02-12 22:18:00 -0600

    setup visit relationships

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,6 +1,12 @@
 class Visit < ApplicationRecord
-    validates :viewer_id, presence: true
+    validates :viewer_id, :art_id, :date, presence: true
+
+    def viewer 
+        Viewer.find_by(id: self.viewer_id)
+    end
+
+    def art
+        Art.find_by(id: self.art_id)
+    end
     
-
-
 end
