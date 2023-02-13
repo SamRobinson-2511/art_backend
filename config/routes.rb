46e4d2a75fe2a70aa4ad7a4c471e252ae79c2a13
Sam Robinson <110217039+SamRobinson-2511@users.noreply.github.com>
2023-02-12 23:09:59 -0600

    some viewer authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :arts
   resources :visits
   resources :viewers
+
+  post '/login', to: 'sessions#create', as: 'login'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
